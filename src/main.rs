5a5c49e8bda301e2321f101ae581f10b0f724b8d
Yamil-Serrano <yamil.serrano1@upr.edu>
2025-05-08 22:54:21 -0400

    Links QR code generator. DO NOT scan the test QR code

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,63 @@
 
-use std::io;                // For handling input/output operations 
+use std::io;              // For handling input/output operations 
 use rand::Rng;              // For generating random numbers 
 use qrcode::QrCode;         // For creating QR codes
 use image::Luma;            // For handling the image format (black and white in this case)
+use colored::Colorize;
 
 fn main() {
-    println!("Enter your Network Name: ");
+    println!("{}\nWellcome to RustyBite please choose an option:\n1. Create a WIFI QR Code\n2. Create a Link QR Code", "-".repeat(50));
+    let mut option = String::new();
+    io::stdin().read_line(& mut option).expect("Incorrect option, please try again.");
+    let option = option.trim();
 
-    // Create a mutable (changeable) empty string to store the network name
-    let mut network_name = String::new();
+    if option == "1" {
+        wifi_qr_code();
+    }
+
+    else if option == "2" {
+        link_qr_code();
+    }
+
+    else {
+        println!("{}","Incorrect option, please try again.".red());
+        main();
+    }
+
     
-    // Read user input into the network_name variable
-    io::stdin().read_line(&mut network_name).expect("Error reading line");
+}
+fn link_qr_code() {
+    println!("{}\nEnter your link: ", "-".repeat(50));
+    let mut link = String::new();
+    io::stdin().read_line(&mut link).expect("Error reading line");
+    let link = link.trim();
 
+    match QrCode::new(link.as_bytes()) {
+        Ok(code) => {
+            let image = code.render::<Luma<u8>>().build();
+            match image.save("link_qr.png") {
+                Ok(_) => println!("{}\n{}","QR code saved".green(),"-".repeat(50)),
+                Err(e) =>{ 
+                    let error = format!("Error saving the QR code: {}", e);
+                    println!("{}",error.red());
+                } 
+            }
+        },
+        Err(e) => {
+            let error = format!("Error creating QR code: {}", e);
+            println!("{}",error.red());
+        }
+    }
+}
+
+fn wifi_qr_code() {
+
+    println!("{}\nEnter your Network Name: ", "-".repeat(50));
+    let mut network_name = String::new();
+    io::stdin().read_line(&mut network_name).expect("Error reading line");
     let network_name = network_name.trim(); //delete the /n at the end of the SSID 
 
     println!("Password length: ");
-
     let mut password_length = String::new();
     io::stdin().read_line(&mut password_length).expect("Error reading line");
     
@@ -30,6 +71,7 @@ fn main() {
     let characters_length = characters.len();
 
     // Create a random number generator (RNG) a machine that creates a random number each time you call it
+    #[allow(deprecated)]
     let mut rng = rand::thread_rng();
     
     // Create a string to hold the password with pre-allocated capacity
@@ -38,7 +80,7 @@ fn main() {
     // Loop to generate each character of the password
     for _ in 0..password_length {
         // Generate a random index between 0 and characters_length-1
-        let index = rng.gen_range(0..characters_length);
+        let index = rng.random_range(0..characters_length);
         
         // Get the character at that index
         let character = characters[index];
@@ -64,13 +106,17 @@ fn main() {
             
             // Try to save the image and handle potential errors
             match image.save("wifi_qr.png") {
-                Ok(_) => println!("QR code saved"),
-                Err(e) => println!("Error saving the QR code: {}", e),
+                Ok(_) => println!("{}\n{}","QR code saved".green(),"-".repeat(50)),
+                Err(e) =>{ 
+                    let error = format!("Error saving the QR code: {}", e);
+                    println!("{}",error.red());
+                } 
             }
         },
         // If QR code creation fails
         Err(e) => {
-            println!("Error creating QR code: {}", e);
+            let error = format!("Error creating QR code: {}", e);
+            println!("{}",error.red());
         }
     }
 }
